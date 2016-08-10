84f91d84ab598c4cf83363068c9de546250c5577
Alfonso Nava Morales <alfonsonavamorales@gmail.com>
2016-08-10 13:17:39 +0200

    primera subida

---
+++ Assets/Scripts/ballMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballMovement : MonoBehaviour {
+
+	public LayerMask collisionMask;
+	public float speed=2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+		Debug.DrawRay(transform.position, transform.forward, Color.green);
+
+
+		if (Physics.Raycast (ray, out hit, Time.deltaTime * speed + .1f, collisionMask)) {
+			Vector3 reflectDir = Vector3.Reflect (ray.direction, hit.normal);
+			float rot = 90 - Mathf.Atan2 (reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3 (0, rot, 0);
+		}
+	}
+
+}
---
+++ Assets/Scripts/stickMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stickMovement : MonoBehaviour {
+
+	public GameObject target;
+	public float vel=2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftArrow))
+			rotate (1);
+		else if (Input.GetKey (KeyCode.RightArrow))
+			rotate (-1);
+		
+	}
+
+	void rotate(int dir){
+		transform.RotateAround (target.transform.position, Vector3.forward, vel*dir*Time.deltaTime);		
+	}
+}
