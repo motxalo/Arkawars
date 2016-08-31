4179abca7381177d68715110c992ec9150cacced
Alfonso <alfonsonavamorales@gmail.com>
2016-08-31 05:31:28 +0200

    Virtual Controller

---
+++ Assets/Scripts/VirtualLeftJoystick.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class VirtualLeftJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector3 inputVector;
+
+    private void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            inputVector = new Vector3(pos.x * 2 + 1, 0, 0);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            //Mover Imagen del Joystick
+            joystickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImg.rectTransform.sizeDelta.x / 3), 0, 0);
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public float Horizontal()
+    {
+        return inputVector.x;
+    }
+}
--- Assets/Scripts/stickMovement.cs
+++ Assets/Scripts/stickMovement.cs
@@ -8,23 +8,36 @@ public class stickMovement : MonoBehaviour {
 	public float rotateSpeed = 2f;
 	public float maxRotation = 45;
 	private float rotation = 0;
-	// Use this for initialization
-	void Start () {
+    public VirtualLeftJoystick leftJoystick;
+    public VirtualRightJoystick rightJoystick;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.LeftArrow))
-			Rotate (1);
-		else if (Input.GetKey (KeyCode.RightArrow))
+        if (leftJoystick.Horizontal()>0)
+            Rotate(-1);
+        else if (leftJoystick.Horizontal() < 0)
+            Rotate(1);
+
+        if (rightJoystick.Horizontal() > 0)
+            RotatePlayer(-1f);
+        else if (rightJoystick.Horizontal() < 0)
+            RotatePlayer(1f);
+
+        /*if (Input.GetKey (KeyCode.LeftArrow))
+            Rotate (1);
+        else if (Input.GetKey (KeyCode.RightArrow))
 			Rotate (-1);
-		if (Input.GetKey (KeyCode.UpArrow)){
+
+        if (Input.GetKey (KeyCode.UpArrow)){
 			RotatePlayer(1f);
 		}else if (Input.GetKey (KeyCode.DownArrow)){
 			RotatePlayer(-1f);
-		}
+		}*/
 		
 	}
 
