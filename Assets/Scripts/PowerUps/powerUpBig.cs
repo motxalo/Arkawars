9c33c3b8cba80701f0a18a51d68818691db883c6
Alfonso Nava Morales <alfonsonavamorales@gmail.com>
2016-09-09 13:26:55 +0200

    primer powerup

---
+++ Assets/Scripts/PowerUps/powerUpBig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class powerUpBig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(transform.localScale.x*2, transform.localScale.y, transform.localScale.z);
+		Invoke ("Destroy", 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Destroy() {
+		Debug.Log ("se acabo");
+		transform.localScale = new Vector3(transform.localScale.x/2, transform.localScale.y, transform.localScale.z);
+	}
+}
